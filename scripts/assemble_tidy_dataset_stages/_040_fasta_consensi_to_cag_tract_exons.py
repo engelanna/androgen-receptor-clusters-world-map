e487f529ccba9ff2215a674699195c12e666e72b
Anna Julia Engel <anna.julia.engel@gmail.com>
2022-07-17 17:18:51 +0200

    Skip fasta headers on cag_tract_exon writes

--- scripts/assemble_tidy_dataset_stages/_040_fasta_consensi_to_cag_tract_exons.py
+++ scripts/assemble_tidy_dataset_stages/_040_fasta_consensi_to_cag_tract_exons.py
@@ -16,14 +16,13 @@ def fasta_consensi_to_cag_tract_exons(
     for input_fasta_file_name in sorted(just_the_nonhidden_files(input_fasta_dir_path)):
         input_fasta_file_path = f"{input_fasta_dir_path}/{input_fasta_file_name}"
 
-        exon_list = fasta_dict_loader(input_fasta_file_path).values()
+        exon_list = list(fasta_dict_loader(input_fasta_file_path).values())
         cag_exon = extract_exon_with_the_most_cag_repeats(exon_list)
 
         fasta_header = input_fasta_file_name.rsplit(".", 1)[0]
         output_fasta_file_path = f"{output_fasta_dir_path}/{fasta_header}.fasta"
 
         with open(output_fasta_file_path, "w") as output_fasta_file:
-            output_fasta_file.write(f">{fasta_header}\n")
             output_fasta_file.write(f"{cag_exon}\n")
 
         print(f"wrote {output_fasta_file_path}")
