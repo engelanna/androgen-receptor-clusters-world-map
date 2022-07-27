347c7e71548cebe0b8a490da956cb18607c2b3b8
Anna Julia Engel <anna.julia.engel@gmail.com>
2022-07-28 01:06:50 +0200

    Shorten names + add colors to map dataframes

--- scripts/_060_clustering_results_to_map_dataframes.py
+++ scripts/_060_clustering_results_to_map_dataframes.py
@@ -1,9 +1,10 @@
 import pandas as pd
+import pydeck
 
 from src.loaders import just_the_nonhidden_files
 
 
-file_name_to_sample_name = lambda file_name: file_name.split(".")[0]
+file_name_to_sample_name = lambda column: column.split(".")[0]
 
 
 def clustering_results_to_map_dataframes(
@@ -11,25 +12,23 @@ def clustering_results_to_map_dataframes(
     populations_lat_long=pd.read_csv("assets/tsv/populations_lat_long.tsv", sep="\t"),
     clusters_dir_path="assets/tsv/clustering_output",
 ):
+
     for cluster_tsv_file_name in sorted(just_the_nonhidden_files(clusters_dir_path)):
         clusters_df = pd.read_csv(
             f"{clusters_dir_path}/{cluster_tsv_file_name}",
             names=["Cluster representative", "Clustered sequence"],
             sep="\t",
         )
+        for colname in list(clusters_df):
+            clusters_df[colname] = clusters_df[colname].apply(file_name_to_sample_name)
+
+        color_lookup = pydeck.data_utils.assign_random_colors(
+            clusters_df["Cluster representative"]
+        )
+        clusters_df["Color"] = clusters_df.apply(
+            lambda row: color_lookup.get(row["Cluster representative"]), axis=1
+        )
         print(clusters_df)
-    # output_fasta_file = open(output_fasta_file_path, "w")
-
-    #     sequence_header, sequence = list(fasta_loader(input_fasta_file_path).items())[0]
-    #     sequence_header = sequence_header[1:]
-    #     sequence_header = (
-    #         f">{cluster_tsv_file_name.replace('.fasta', '')}:{sequence_header}"
-    #     )
-    #     # at this point, the header is e.g. >filename:chrX:67543872-67730762
-    #     output_fasta_file.write(f"{sequence_header}\n")
-    #     output_fasta_file.write(f"{sequence}\n")
-
-    # output_fasta_file.close()
 
 
 if __name__ == "__main__":
