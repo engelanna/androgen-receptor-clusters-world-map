545f4749fcb8e171156d8852c68a741d90a69f7b
Anna Julia Engel <anna.julia.engel@gmail.com>
2022-07-27 10:04:57 +0200

    Commit initial map files (html, tsv)

--- scripts/assemble_tidy_dataset_stages/_050_multiple_fasta_to_single_multifasta.py
+++ scripts/assemble_tidy_dataset_stages/_050_multiple_fasta_to_single_multifasta.py
@@ -1,12 +1,13 @@
 import os
 import subprocess
 
-from src.loaders import just_the_nonhidden_files
+from src.loaders import just_the_nonhidden_files, FastaDictFromFile
 
 
 def multiple_fasta_to_single_multifasta(
-    input_fasta_dir_path="assets/fasta/cag_tract_exons",
-    output_fasta_file_path="assets/fasta/clustering_input/cd_hit_input.fasta",
+    input_fasta_dir_path="assets/fasta/consensus_sequences",
+    output_fasta_file_path="assets/fasta/clustering_input/mmseqs_input.fasta",
+    fasta_loader=FastaDictFromFile(),
 ):
     output_fasta_file = open(output_fasta_file_path, "w")
 
@@ -14,10 +15,14 @@ def multiple_fasta_to_single_multifasta(
         input_fasta_file_path = f"{input_fasta_dir_path}/{input_fasta_file_name}"
         print(f"processing {input_fasta_file_path}")
 
-        sequence = open(input_fasta_file_path, "r").readlines()[1]
-        sequence_header = input_fasta_file_name.replace(".fasta", "")
-
-        output_fasta_file.writelines([f">{sequence_header}\n", f"{sequence}"])
+        sequence_header, sequence = list(fasta_loader(input_fasta_file_path).items())[0]
+        sequence_header = sequence_header[1:]
+        sequence_header = (
+            f">{input_fasta_file_name.replace('.fasta', '')}:{sequence_header}"
+        )
+        # at this point, the header is e.g. >filename:chrX:67543872-67730762
+        output_fasta_file.write(f"{sequence_header}\n")
+        output_fasta_file.write(f"{sequence}\n")
 
     output_fasta_file.close()
 
