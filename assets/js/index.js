e6e724803c4835e97f604e9d4259ce76dee8cb0e
Anna Julia Engel <anna.julia.engel@gmail.com>
2022-07-27 13:56:26 +0200

    Set map min/max zoom and start coords

--- assets/js/index.js
+++ assets/js/index.js
@@ -8,12 +8,14 @@
  The source code below is the example from the leaflet start page.
  */
 
-var map = L.map('map').setView([51.505, -0.09], 13);
+var map = L.map("map", { minZoom: 2, maxZoom: 6 }).setView([0, 0], 3);
 
-L.tileLayer('https://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-		attribution: '&copy; <a href="https://osm.org/copyright">OpenStreetMap</a> contributors'
+L.tileLayer("https://{s}.tile.osm.org/{z}/{x}/{y}.png", {
+  attribution:
+    '&copy; <a href="https://osm.org/copyright">OpenStreetMap</a> contributors',
 }).addTo(map);
 
-L.marker([51.5, -0.09]).addTo(map)
-		.bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-		.openPopup();
+L.marker([51.5, -0.09])
+  .addTo(map)
+  .bindPopup("A pretty CSS3 popup.<br> Easily customizable.")
+  .openPopup();
