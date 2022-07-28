8e17529fa1aae983476a5e431500c952d9482bb1
Anna Julia Engel <anna.julia.engel@gmail.com>
2022-07-28 18:06:22 +0200

    Switch to PascalCase names

--- src/pydeck/arc_layer.py
+++ src/pydeck/arc_layer.py
@@ -8,17 +8,22 @@ def build_deck_renderer(minimum_sequence_identity: float):
         sep="\t",
     )
 
-    color_lookup = pydeck.data_utils.assign_random_colors(df["Cluster name"])
-    df["Color"] = df.apply(lambda row: color_lookup.get(row["Cluster name"]), axis=1)
+    color_lookup = pydeck.data_utils.assign_random_colors(df["ClusterName"])
+    df["ClusterColor"] = df.apply(
+        lambda row: color_lookup.get(row["ClusterName"]), axis=1
+    )
+    color_lookup = pydeck.data_utils.assign_random_colors(df["SampleName"])
+    df["SampleColor"] = df.apply(
+        lambda row: color_lookup.get(row["SampleName"]), axis=1
+    )
 
-    # Specify a deck.gl ArcLayer
     arc_layer = pydeck.Layer(
         "ArcLayer",
         data=df,
-        get_source_position=["Longitude", "Latitude"],
-        get_target_position=[5, 20],
-        get_source_color=[255, 100, 0],
-        get_target_color="Color",
+        get_source_position=["ClusterLongitude", "ClusterLatitude"],
+        get_target_position=["PopulationLongitude", "PopulationLatitude"],
+        get_source_color="ClusterColor",
+        get_target_color="SampleColor",
         pickable=True,
         auto_highlight=True,
     )
