c367b64a10dd3162881071c892121c7005b8b563
Anna Julia Engel <anna.julia.engel@gmail.com>
2022-07-29 16:33:58 +0200

    Try out ColumnLayer

--- src/pydeck/arc_layer.py
+++ src/pydeck/arc_layer.py
@@ -7,42 +7,49 @@ def build_deck_renderer(minimum_sequence_identity: float):
         f"assets/tsv/map_ready_dataframes/min-seq-id-{minimum_sequence_identity:.3f}.tsv",
         sep="\t",
     )
-
     color_lookup = pydeck.data_utils.assign_random_colors(df["ClusterName"])
     df["Color"] = df.apply(lambda row: color_lookup.get(row["ClusterName"]), axis=1)
+    df["Elevation"] = pd.Series([1000 for x in range(len(df.index))])
 
     # get_source_position=["Longitude", "Latitude"],
     # get_source_color="Color",
-    arc_layer = pydeck.Layer(
-        "ScatterplotLayer",
-        df,
-        pickable=True,
-        opacity=0.8,
-        stroked=True,
-        radius_min_pixels=2,
-        line_width_min_pixels=1,
+    layer = pydeck.Layer(
+        "ColumnLayer",
+        data=df,
         get_position=["Longitude", "Latitude"],
-        radius="exits_radius",
+        get_elevation="Elevation",
+        elevation_scale=10000,
+        radius=50000,
         get_fill_color="Color",
-        get_line_color="Color",
+        pickable=True,
+        auto_highlight=True,
     )
-
     view_state = pydeck.ViewState(
-        latitude=0.0,
-        longitude=0.0,
+        longitude=0,
+        latitude=0,
         zoom=0,
+        min_zoom=0,
+        max_zoom=15,
+        pitch=0,
+        bearing=0,
     )
-
-    TOOLTIP_TEXT = {
-        "html": "{S000} jobs <br /> Home of commuter in red; work location in green"
+    tooltip = {  # either use "text", or "html"
+        "html": "<a href={SampleLink}>{SampleName}</a>",
+        # "style": {"color": "red"},
+        "className": "interactive",
     }
-
     # deck_renderer = \
     return pydeck.Deck(
-        arc_layer,
+        layer,
         initial_view_state=view_state,
-        map_style="dark",
-        map_provider="mapbox",
-        tooltip=TOOLTIP_TEXT,
+        map_style="road",
+        tooltip=tooltip,
     )
-    # deck_renderer.to_html("arc_layer.html")
+    # deck_renderer.to_html("layer.html")
+
+
+"""
+tooltip (bool or dict of {str: str}, default True) – If True/False,
+toggles a default tooltip on visualization hover.
+
+"""
