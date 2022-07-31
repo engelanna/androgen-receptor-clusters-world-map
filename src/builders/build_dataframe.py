e62ffed15bcd41a3a67b6cf195f60d7522187bc0
Anna Julia Engel <anna.julia.engel@gmail.com>
2022-07-31 16:13:03 +0200

    Include color assignment in df generation

--- src/builders/build_dataframe.py
+++ src/builders/build_dataframe.py
@@ -1,7 +1,4 @@
 import pandas as pd
-import pydeck
-
-from src.loaders import ColorLookup
 
 
 class BuildDataframe:
@@ -10,8 +7,5 @@ class BuildDataframe:
             f"assets/tsv/map_ready_dataframes/min-seq-id-{minimum_sequence_identity}.tsv",
             sep="\t",
         )
-        color_lookup = ColorLookup().assign_colors(df["ClusterName"])
-        df["Color"] = df.apply(lambda row: color_lookup.get(row["ClusterName"]), axis=1)
-        df["Elevation"] = [0.9 if x == [255, 255, 255] else 1.0 for x in df["Color"]]
 
         return df
